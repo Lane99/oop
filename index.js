2ee4919d9163cf5678dff0a0c20ac289b9b4d549
Lane99 <107255046+Lane99@users.noreply.github.com>
2022-09-11 19:51:12 +0200

    kraj

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let person = {
   },
 };
 
-// person.bio();
+person.bio();
 // person.introduceSelf();
 // person['bio']();
 // person['introduceSelf']();
@@ -25,11 +25,32 @@ let person = {
 
 //typeof person[choice] == 'function' ? person[choice]() : console.log('Oops');
 
-person.eyes = 'brown';
-person['farewell'] = function() {
-  console.log('Dovidjenja');
+// person.eyes = 'brown';
+// person['farewell'] = function() {
+//   console.log('Dovidjenja');
+// }
+
+// person.farewell();
+
+// let key = prompt('Key?');
+// let value = prompt('Value?');
+
+// person[key] = value;
+
+// console.log(Object.entries(person));
+
+function createPerson(name, age) {
+  const obj = {};
+  obj.name = name;
+  obj.age = age;
+  obj.introduce = function() {
+    console.log(`Hi, I'm ${this.name}`);
+  }
+  return obj;
 }
 
-person.farewell();
+const jovica = createPerson('Jovica', 43);
+const mikica = createPerson('Mikica', 40);
 
-console.log(Object.entries(person));
+jovica.introduce();
+mikica.introduce();
